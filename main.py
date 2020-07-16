2eb77d2f48ba77f98c143faad9073bf2cc6d1f14
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-07-16 15:19:11 +1200

    Basic movement and bounds checking

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import pygame
 pygame.init()
 
 # Creates game window
-window = pygame.display.set_mode((500, 500))
+windowSizeX = 500
+windowSizeY = 500
+
+window = pygame.display.set_mode((windowSizeX, windowSizeY))
 
 # Title and Icon
 pygame.display.set_caption("Ben's Python Game")
@@ -17,12 +20,34 @@ pygame.display.set_icon(icon)
 # Set the background color
 window.fill((255, 255, 255))
 
+# Set up for the basic player size/speed/location
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 1
+
+# Main Loop
 running = True
 while running:
 
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
            running = False
+
+    # Movement
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > vel:
+       x -= vel
+    if keys[pygame.K_RIGHT] and x < windowSizeX - width:
+       x += vel
+    if keys[pygame.K_UP] and y > vel:
+       y -= vel
+    if keys[pygame.K_DOWN] and y < windowSizeY - height:
+       y += vel
+       
+    window.fill((255, 255, 255))
+    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
 
     pygame.display.update()
 
